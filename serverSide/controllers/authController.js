eeca877237b4cd0cb86423ae2407d5178c720e99
rohit1620 <malavrohit1620@gmail.com>
2026-03-14 10:01:41 +0530

    login functionality complete

--- serverSide/controllers/authController.js
+++ serverSide/controllers/authController.js
@@ -1,12 +1,16 @@
 const User =require("../models/authSchema");
 const Otp=require("../models/optSchema")
 const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken")
 const sendOtpMail= require("../utils/email")
 
 const generateOpt=()=>{
     return Math.floor(100000 + Math.random()*900000).toString()
 }
 
+const generateToken=(id,role)=>{
+    return jwt.sign({id,role},process.env.JWT_SECRATE,{expiresIn:"30d"})
+}
 const register=async(req,res)=>{
     try {
            const {username,email,password,phone}=req.body;
@@ -29,7 +33,23 @@ const register=async(req,res)=>{
 
 const login=async(req,res)=>{
 try {
-         
+         const {email,password}=req.body;
+         const userExist=await User.findOne({email});
+         if(!userExist){
+            return res.status(400).json({"msg":"Invailid credentials"})
+         }
+         const isMatch=await bcrypt.compare(password,userExist.password);
+         if(!isMatch){
+            return res.status(400).json({"msg":"Invailid credentials"})
+         }
+         if(!userExist.isVerified && userExist.role!=="admin"){
+            const otp=generateOpt();
+            await Otp.findOneAndDelete({email,type:"account varification"})
+            await Otp.create({email,otp,type:"account varification"});
+            await sendOtpMail(email,otp,"account varification");
+            res.status(400).json({"msg":"Account not verified",needsVerification:true,email:userExist.email})
+         }
+         res.status(200).json({"msg":"Login successfully",id:userExist._id,name:userExist.name,role:userExist.role,email:userExist.email,token:generateToken(userExist._id,userExist.role)})
 } catch (error) {
      res.status(500).json({"msg":"internal error",error})
 }
@@ -44,7 +64,7 @@ const varify=async(req,res)=>{
         }
         const user=await User.findOneAndUpdate({email},{isVerified:true},{new:true});
         await Otp.deleteOne({_id:validOtp._id})
-        res.status(200).json({"msg":"email verified"})
+        res.status(200).json({"msg":"email verified successfully"})
             
     } catch (error) {
          res.status(500).json({"msg":"internal error",error})
