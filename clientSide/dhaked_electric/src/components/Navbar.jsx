5341d6c3e49e36db46f23ac4725c20a77c0ed225
rohit1620 <malavrohit1620@gmail.com>
2026-03-18 11:39:38 +0530

    add something

--- clientSide/dhaked_electric/src/components/Navbar.jsx
+++ clientSide/dhaked_electric/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCartArrowDown,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div className="fixed w-full top-0 z-50 bg-black text-white flex h-15 justify-around items-center ">
@@ -17,18 +23,20 @@ const Navbar = () => {
             placeholder="Search products..."
             class=" hidden md:block px-4 py-2 w-38 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white placeholder-black h-6 text-black"
           />
-          <ul className="fixed md:static top-15  left-0 h-screen md:h-max bg-black w-full md:w-max flex flex-col items-center p-10 md:p-0 md:flex-row gap-5">
+          <ul
+            className={`fixed md:static top-15  left-0 h-screen md:h-max bg-black w-full md:w-max flex flex-col items-center p-10 md:p-0 md:flex-row gap-5 transform transition-transform duration-300 ${open ? "translate-x-0" : "-translate-x-full"}`}
+          >
             <Link to="/">
-              <li>Home</li>
+              <li onClick={() => setOpen(!open)}>Home</li>
             </Link>
             <Link to="/product">
-              <li>Product</li>
+              <li onClick={() => setOpen(!open)}>Product</li>
             </Link>
             <Link to="/singup">
-              <li>Singup</li>
+              <li onClick={() => setOpen(!open)}>Singup</li>
             </Link>
             <Link to="/login">
-              <li>Login</li>
+              <li onClick={() => setOpen(!open)}>Login</li>
             </Link>
           </ul>
         </div>
@@ -42,8 +50,12 @@ const Navbar = () => {
             <h2>profile</h2>
           </Link>
         </div>
-        <h3 className="md:hidden">
-          <FontAwesomeIcon icon={faBars} />
+        <h3 className="md:hidden" onClick={() => setOpen(!open)}>
+          {open ? (
+            <FontAwesomeIcon icon={faCircleXmark} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
         </h3>
       </div>
     </>
