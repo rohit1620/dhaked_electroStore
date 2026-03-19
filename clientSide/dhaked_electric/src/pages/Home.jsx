7d3205ba51af210232b579ad9240044938a66bac
rohit1620 <malavrohit1620@gmail.com>
2026-03-19 08:56:56 +0530

    add about store

--- clientSide/dhaked_electric/src/pages/Home.jsx
+++ clientSide/dhaked_electric/src/pages/Home.jsx
@@ -39,23 +39,70 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="relative">
-      <div
-        onClick={handlePrev}
-        className="absolute text-white bg-black p-2 top-[50%] left-5"
-      >
-        {"<"}
+    <div>
+      <div className="relative">
+        <div
+          onClick={handlePrev}
+          className="absolute text-white bg-black p-2 top-[50%] left-5"
+        >
+          {"<"}
+        </div>
+        <img
+          src={datas[index].image_url}
+          alt="image not found"
+          className="w-full h-screen"
+        />
+        <div
+          onClick={handleNext}
+          className="absolute text-white bg-black p-2 top-[50%] right-5"
+        >
+          {">"}
+        </div>
       </div>
-      <img
-        src={datas[index].image_url}
-        alt="image not found"
-        className="w-full h-screen"
-      />
-      <div
-        onClick={handleNext}
-        className="absolute text-white bg-black p-2 top-[50%] right-5"
-      >
-        {">"}
+      {/* about store section */}
+      <div className="md:flex px-8 md:my-12 h-screen">
+        <div className="md:w-1/2 pt-5">
+          <h2 className="font-bold text-3xl text-center mb-2 italic">
+            About Our Store
+          </h2>
+          <p className="text-justify italic">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
+            rerum quasi sunt animi natus asperiores minus quaerat ad quas at
+            eaque nisi fuga culpa voluptatem, eum, dolorum tempore consequuntur
+            odio! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+            soluta vitae in ratione, laudantium, ab unde iusto accusantium, iste
+            ipsam tempora nam veniam quo blanditiis facere voluptate? Vitae,
+            provident repudiandae. Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Natus laborum, nostrum cupiditate iure enim
+            officiis ipsum itaque minima error iste, excepturi delectus, eius
+            rerum et? Obcaecati sapiente similique harum! Distinctio. Lorem
+            ipsum dolor, sit amet consectetur adipisicing elit. Possimus
+            mollitia perferendis saepe nobis reprehenderit quidem corrupti
+            necessitatibus illo repellendus id nemo praesentium modi, odit
+            maxime, voluptatibus obcaecati, iste harum perspiciatis?
+          </p>
+        </div>
+        <div className="w-1/2 hidden md:block">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfIKZtg95BcJfA5z8NbBD1Bl-rAuMPVWKXfQ&s"
+            alt=""
+            className="w-full h-[400px]"
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
       </div>
     </div>
   );
