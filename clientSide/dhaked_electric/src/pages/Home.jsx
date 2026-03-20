dae79cdc4b8f90a4cbe91b5ae7d4a200e70cf2d5
rohit1620 <malavrohit1620@gmail.com>
2026-03-20 09:28:06 +0530

    our service section complet

--- clientSide/dhaked_electric/src/pages/Home.jsx
+++ clientSide/dhaked_electric/src/pages/Home.jsx
@@ -60,48 +60,88 @@ const Home = () => {
         </div>
       </div>
       {/* about store section */}
-      <div className="md:flex px-8 md:my-12 h-screen">
+      <div className="md:flex px-8 md:my-10">
         <div className="md:w-1/2 pt-5">
           <h2 className="font-bold text-3xl text-center mb-2 italic">
             About Our Store
           </h2>
-          <p className="text-justify italic">
+          <p className="text-justify italic pb-4 sm:pb-0">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
             rerum quasi sunt animi natus asperiores minus quaerat ad quas at
             eaque nisi fuga culpa voluptatem, eum, dolorum tempore consequuntur
             odio! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
             soluta vitae in ratione, laudantium, ab unde iusto accusantium, iste
             ipsam tempora nam veniam quo blanditiis facere voluptate? Vitae,
-            provident repudiandae. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Natus laborum, nostrum cupiditate iure enim
-            officiis ipsum itaque minima error iste, excepturi delectus, eius
-            rerum et? Obcaecati sapiente similique harum! Distinctio. Lorem
-            ipsum dolor, sit amet consectetur adipisicing elit. Possimus
-            mollitia perferendis saepe nobis reprehenderit quidem corrupti
-            necessitatibus illo repellendus id nemo praesentium modi, odit
-            maxime, voluptatibus obcaecati, iste harum perspiciatis?
+            provident repudiandae.
+            <span className="hidden md:inline">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
+              laborum, nostrum cupiditate iure enim officiis ipsum itaque minima
+              error iste, excepturi delectus, eius rerum et? Obcaecati sapiente
+              similique harum! Distinctio. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Possimus mollitia perferendis saepe
+              nobis reprehenderit quidem corrupti necessitatibus illo
+              repellendus id nemo praesentium modi, odit maxime, voluptatibus
+              obcaecati, iste harum perspiciatis?
+            </span>
           </p>
         </div>
         <div className="w-1/2 hidden md:block">
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfIKZtg95BcJfA5z8NbBD1Bl-rAuMPVWKXfQ&s"
+            src="https://3.imimg.com/data3/TB/KQ/MY-238885/videocon-ha-500x500.jpg"
             alt=""
             className="w-full h-[400px]"
           />
         </div>
       </div>
-      <div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
+      {/* our services section  */}
+      {/* className="bg-pink-100" */}
+      <div className="bg-pink-100 pt-4">
+        <div className="w-[85%] m-auto ">
+          <h1 className="text-4xl font-bold italic text-center mb-8  ">
+            Our Services
+          </h1>
+          <div className="grid  grid-cols-[repeat(1,1fr)] lg:grid-cols-[repeat(4,1fr)] sm:grid-cols-[repeat(2,1fr)] gap-x-5 gap-y-6 sm:gap-y-0">
+            <div className="shadow-service bg-yellow-300">
+              <img
+                src="https://png.pngtree.com/background/20230808/original/pngtree-digital-devices-for-productivity-laptop-mobile-phone-and-tablet-pc-photo-picture-image_4512002.jpg"
+                alt=""
+                className="w-full h-[350px]"
+              />
+              <h3 className="text-center text-xl font-bold italic text-pink-600 mb-4 mt-2">
+                Laptop & Mobiles
+              </h3>
+            </div>
+            <div className="shadow-service bg-yellow-300">
+              <img
+                src="https://images.hindustantimes.com/tech/img/2024/03/23/1600x900/sgddxgfhhc_1711231456166_1711231463102.jpg"
+                alt=""
+                className="w-full h-[350px] "
+              />
+              <h3 className="text-center text-xl font-bold italic text-pink-600 mb-4 mt-2">
+                Laptop & Mobiles
+              </h3>
+            </div>
+            <div className="shadow-service bg-yellow-300">
+              <img
+                src="https://www.shutterstock.com/image-photo/home-appliances-tv-refrigerator-washing-260nw-2248825351.jpg"
+                alt=""
+                className="w-full h-[350px] "
+              />
+              <h3 className="text-center text-xl font-bold italic text-pink-600 mb-4 mt-2">
+                Laptop & Mobiles
+              </h3>
+            </div>
+            <div className="shadow-service bg-yellow-300">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSixzSyETiHkN2IjpHbF02yRQsU-oXtBk3Ujg&s"
+                alt=""
+                className="w-full h-[350px] "
+              />
+              <h3 className="text-center text-xl font-bold italic text-pink-600 mb-4 mt-2">
+                Laptop & Mobiles
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
